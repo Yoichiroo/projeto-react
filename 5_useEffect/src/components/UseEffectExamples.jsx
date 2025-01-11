07666367bde2e8741748ead43be90fe033e3803d
Lídia Torres <109477475+Yoichiroo@users.noreply.github.com>
2025-01-11 15:39:29 -0300

    useEffect - Aula 5

--- 5_useEffect/src/components/UseEffectExamples.jsx
+++ 5_useEffect/src/components/UseEffectExamples.jsx
@@ -1,8 +1,29 @@
-import React from 'react'
+import { useState, useEffect } from 'react'
 
 const UseEffectExamples = () => {
+    const [count, setCount] = useState(0)
+
+    useEffect(() => {
+        console.log("useEffect 1")
+    })
+
+    // dependencia vazia ou array vazio 
+
+    useEffect(() => {
+        console.log("useEffect 2")
+    }, [])
+
+    // com dependencia
+
+    useEffect(() => {
+        console.log("useEffect 3")
+    }, [count])
+
     return (
-        <div>UseEffectExamples</div>
+        <>
+            <p>{count}</p>
+            <button onClick={() => setCount(count + 1)}>Aumentar contagem</button>
+        </>
     )
 }
 
